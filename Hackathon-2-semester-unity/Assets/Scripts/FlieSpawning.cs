4c21ab7487bb482373873d26b03d792ffe504c31
GITTot04 <112081156+GITTot04@users.noreply.github.com>
2025-05-01 15:31:13 +0200

    Scener er lavet men ikke færdige

--- Hackathon-2-semester-unity/Assets/Scripts/FlieSpawning.cs
+++ Hackathon-2-semester-unity/Assets/Scripts/FlieSpawning.cs
@@ -2,31 +2,23 @@ using UnityEngine;
 using System.Collections;
 
 public class FlieSpawning : MonoBehaviour
-
 {
     public GameObject[] Flies = new GameObject[10];
     int numberOfFlies;
-    IEnumerator FlieSpawningCoroutine()
-
-    {
-
-        while (numberOfFlies < Flies.Length)
-        {
-            Flies[numberOfFlies].SetActive(true);
-            numberOfFlies++;   
-            yield return new WaitForSeconds(1);
-           
-        }
-    }
-
-// Start is called once before the first execution of Update after the MonoBehaviour is created
-void Start()
+    public GameObject nextArrow;
+    void Start()
     {
         StartCoroutine(FlieSpawningCoroutine());
     }
-    // Update is called once per frame
-    void Update()
+
+    IEnumerator FlieSpawningCoroutine()
     {
-        
+        while (numberOfFlies < Flies.Length)
+        {
+            Flies[numberOfFlies].SetActive(true);
+            numberOfFlies++;
+            yield return new WaitForSeconds(1);
+        }
+        nextArrow.SetActive(true);
     }
 }
--- Hackathon-2-semester-unity/Assets/Scripts/Smacking.cs
+++ Hackathon-2-semester-unity/Assets/Scripts/Smacking.cs
@@ -10,19 +10,27 @@ public class Smacking : MonoBehaviour
     public Sprite deadFlie;
     TouchState touch;
     Vector2 touchStartPos;
+    int deadFlies;
+    public GameObject nextArrow;
     private void OnSmacking()
     { 
         touch = Touchscreen.current.primaryTouch.value;
         touchStartPos = Camera.main.ScreenToWorldPoint(new Vector2(touch.startPosition.x, touch.startPosition.y));
         FlySwatter.transform.position = touchStartPos;
         foreach (GameObject intertactableObject in intertactableObject)
-        if(intertactableObject !=null)
-
-
-        if (touchStartPos.x + 0.75 > intertactableObject.transform.position.x && touchStartPos.x - 0.75 < intertactableObject.transform.position.x && touchStartPos.y + 0.75 > intertactableObject.transform.position.y && touchStartPos.y - 0.75 < intertactableObject.transform.position.y)
         {
-                intertactableObject.GetComponent<SpriteRenderer>().sprite = deadFlie;
-
+            if (intertactableObject != null)
+            {
+                if (touchStartPos.x + 0.75 > intertactableObject.transform.position.x && touchStartPos.x - 0.75 < intertactableObject.transform.position.x && touchStartPos.y + 0.75 > intertactableObject.transform.position.y && touchStartPos.y - 0.75 < intertactableObject.transform.position.y)
+                {
+                    intertactableObject.GetComponent<SpriteRenderer>().sprite = deadFlie;
+                    deadFlies++;
+                    if (deadFlies == 10)
+                    {
+                        nextArrow.SetActive(true);
+                    }
+                }
+            }
         }
     }
 }
